e08c36d3fe0e45edbb22ff1ebbcb51d95d9dfe3f
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2022-07-12 21:48:48 -0700

    index.js
    wbg sub pictures

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import { Parallax, Background } from 'react-parallax';
 import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import "./indexstyle.css";
 import {
-    TopTitleBox, TopTitle, TopDescriptionBox, TopDescription, MSContainer, MStitle, MSdescription
+    TopTitleBox, TopTitle, TopDescriptionBox, TopDescription, MSContainer, MStitle, MSdescription,
+    MidTitleBox, MidTitle, MidDescBox, MidDesc, WbgBox
 } from "./indexstyle";
 
 const Home = () => {
@@ -22,12 +23,13 @@ const Home = () => {
                     <TopDescription>Office hours: by appointment only due to COVID-19</TopDescription>
                 </TopDescriptionBox>
             </Parallax>
+
             <MSContainer>
                 <MDBRow className="MSRow">
                     <MDBCol className="col-12 col-sm-8"><img class="MSmain img-fluid"
                         src={process.env.PUBLIC_URL + "/images/home-ms-main.jpg"} alt="Glasses-Showcase"
                     /></MDBCol>
-                    <MDBCol className="col-12 col-sm-4">
+                    <MDBCol className="MSdescBox col-12 col-sm-4">
                         <MSdescription><MStitle>MISSION STATEMENT:</MStitle></MSdescription>
                         <MSdescription>The mission is simple: provide quality eye care with a personal touch and excellent customer
                             service that patients will want to return year after year.
@@ -50,6 +52,35 @@ const Home = () => {
                     /></MDBCol>
                 </MDBRow>
             </MSContainer>
+
+            <Parallax className="home-mid-bg" strength={750}>
+                <Background>
+                    <img className="home-midimg" src={process.env.PUBLIC_URL + "/images/home-middle.jpg"} alt="glasses-closeup" /></Background>
+                <MidTitleBox>
+                    <MidTitle>WHY BUY GLASSES FROM YOUR OPTOMETRIST?</MidTitle>
+                </MidTitleBox>
+                <MidDescBox>
+                    <MidDesc>Because every pair of quality glasses will achieve that elusive great fit and look.</MidDesc>
+                </MidDescBox>
+            </Parallax>
+
+            <WbgBox>
+                <MDBRow className="WbgRow">
+                    <MDBCol className="col-6 col-lg-3 col-md-3"><img class="WbgSub img-fluid"
+                        src={process.env.PUBLIC_URL + "/images/home-wbg-1.jpg"} alt="Glasses-Incase"
+                    /></MDBCol>
+                    <MDBCol className="col-6 col-lg-3 col-md-3"><img class="WbgSub img-fluid"
+                        src={process.env.PUBLIC_URL + "/images/home-wbg-2.jpg"} alt="Protractor"
+                    /></MDBCol>
+                    <MDBCol className="col-6 col-lg-3 col-md-3"><img class="WbgSub img-fluid"
+                        src={process.env.PUBLIC_URL + "/images/home-wbg-3.jpg"} alt="Microscope"
+                    /></MDBCol>
+                    <MDBCol className="col-6 col-lg-3 col-md-3"><img class="WbgSub img-fluid"
+                        src={process.env.PUBLIC_URL + "/images/home-wbg-4.jpg"} alt="Toolset"
+                    /></MDBCol>
+                </MDBRow>
+            </WbgBox>
+
         </div >
     );
 };
--- src/pages/indexstyle.js
+++ src/pages/indexstyle.js
@@ -10,12 +10,12 @@ justify-content: center;
 margin: 5rem 0 2rem 5rem;
 flex-wrap: wrap;
 @media screen and (max-width: 768px) {
-    margin: 1rem 0 1rem 1rem;
+    margin: 5% 0 5% 5%;
 }
 `;
 
 export const TopTitle = styled.div`
-font-size 37.5px;
+font-size 2.25em;
 letter-spacing: .175em;
 `;
 
@@ -25,28 +25,29 @@ flex-direction: column;
 flex-wrap: nowrap;
 justify-content: center;
 background-color: white;
-height: 30%;
-width: 40%;
+width: 60%;
+max-width: 625px;
 min-width: 425px;
 margin: 0 0 0 5rem;
+padding: 1em 0 1em 0;
 @media screen and (max-width: 768px) {
     min-width: 425px;
-    margin: 1rem 0 1rem 1rem;
+    margin: 5% 0 5% 5%;
 }
 `;
 
 export const TopDescription = styled.div`
-font-size: 17px;
-padding: 0.25rem 0 0.25rem 1rem;
+font-size: 1em;
+padding: .25rem 0 .25rem 1rem;
 `;
 
 export const MSContainer = styled.div`
 display: flex;
 justify-content: center;
 flex-wrap: wrap;
-margin: 5em 5% 0 5%;
+margin: 5em 5% 5% 5%;
 @media screen and (max-width: 768px) {
-   margin: 5% 5% 0 5%;
+   margin: 5% 5% 5% 5%;
 }
 `;
 
@@ -57,5 +58,62 @@ font-size: 13px;
 export const MSdescription = styled.div`
 font-size: 14px;
 padding: 0 0 1em 0;
+`;
+
+export const MidTitleBox = styled.div`
+background-color: white;
+height: 10%;
+width: 80%;
+max-width: 850px;
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 5rem 0 2rem 5rem;
+flex-wrap: wrap;
+@media screen and (max-width: 768px) {
+    min-width: 450px;
+    margin: 5% 0 5% 5%;
+}
+`;
+
+export const MidTitle = styled.div`
+font-size 2.5em;
+letter-spacing: .1em;
+padding: .25em 0 .25em .5em;
+`;
+
+export const MidDescBox = styled.div`
+display: flex;
+flex-direction: column;
+flex-wrap: nowrap;
+justify-content: center;
+background-color: white;
+width: 60%;
+max-width: 750px;
+min-width: 450px;
+margin: 0 0 0 5rem;
+padding: 1em 0 1em 0;
+@media screen and (max-width: 768px) {
+    min-width: 450px;
+    margin: 5% 0 5% 5%;
+}
+`;
+
+export const MidDesc = styled.div`
+font-size: 1em;
+padding: .25rem 0 .25rem 1rem;
+@media screen and (max-width: 768px) {
+    min-width: 450px;
+}
+`;
+
+export const WbgBox = styled.div`
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+margin: 5em 5% 5% 5%;
+@media screen and (max-width: 768px) {
+   margin: 5% 5% 5% 5%;
+}
 `;
 
