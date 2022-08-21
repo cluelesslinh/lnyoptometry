7744d8815a2aa7420a7fbc71bd544f45d947f6a0
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2022-08-20 17:22:03 -0700

    contact.jsx
    contact page complete with working form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,65 +1,49 @@
 /* eslint-disable no-undef */
-import React from 'react';
-import $ from 'jquery';
+import React, { useRef } from 'react';
 import { Parallax, Background } from 'react-parallax';
 import { MDBRow, MDBCol, } from 'mdb-react-ui-kit';
 import MapWrapper from './MapWrapper';
+import emailjs from 'emailjs-com';
 import "./contact.css";
 
 const Contact = () => {
 
-    $(function () {
-        function after_form_submitted(data) {
-            if (data.result == 'success') {
-                $('form#reused_form').hide();
-                $('#success_message').show();
-                $('#error_message').hide();
-            }
-            else {
-                $('#error_message').append('<ul></ul>');
+    const form = useRef();
 
-                jQuery.each(data.errors, function (key, val) {
-                    $('#error_message ul').append('<li>' + key + ':' + val + '</li>');
-                });
-                $('#success_message').hide();
-                $('#error_message').show();
+    const sendEmail = (e) => {
 
-                //reverse the response on the button
-                $('button[type="button"]', $form).each(function () {
-                    $btn = $(this);
-                    label = $btn.prop('orig_label');
-                    if (label) {
-                        $btn.prop('type', 'submit');
-                        $btn.text(label);
-                        $btn.prop('orig_label', '');
-                    }
-                });
+        e.preventDefault();
 
-            }//else
+        emailjs.sendForm('service_ca5xcfs', 'template_gvx2hs4', form.current, 'uBnIuwxqitdcbOtIM')
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    alert("SUCCESS!");
+                },
+                (error) => {
+                    console.log(error.text);
+                    alert("FAILED...", error);
+                }
+            );
+    };
+
+    $(".phone-format").keypress(function (e) {
+        if (e.which !== 8 && e.which !== 0 && (e.which < 48 || e.which > 57)) {
+            return false;
         }
-
-        $('#reused_form').submit(function (e) {
-            e.preventDefault();
-
-            $form = $(this);
-            //show some response on the button
-            $('button[type="submit"]', $form).each(function () {
-                $btn = $(this);
-                $btn.prop('type', 'button');
-                $btn.prop('orig_label', $btn.text());
-                $btn.text('Sending ...');
-            });
-
-
-            $.ajax({
-                type: "POST",
-                url: 'handler.php',
-                data: $form.serialize(),
-                success: after_form_submitted,
-                dataType: 'json'
-            });
-
-        });
+        var curchr = this.value.length;
+        var curval = $(this).val();
+        if (curchr === 3 && curval.indexOf("(") <= -1) {
+            // eslint-disable-next-line no-useless-concat
+            $(this).val("(" + curval + ")" + "-");
+        } else if (curchr === 4 && curval.indexOf("(") > -1) {
+            $(this).val(curval + ")-");
+        } else if (curchr === 5 && curval.indexOf(")") > -1) {
+            $(this).val(curval + "-");
+        } else if (curchr === 9) {
+            $(this).val(curval + "-");
+            $(this).attr('maxlength', '14');
+        }
     });
 
     return (
@@ -144,46 +128,100 @@ const Contact = () => {
 
                     <MDBCol className="col-12"><hr></hr></MDBCol>
 
-                    <MDBCol className="contInq col-12 col-lg-4 col-md-4 col-sm-4">
+                    <MDBCol className="contInq col-12 col-lg-4 col-md-4 col-sm-4 col-xs-12">
                         <div className="contInqTitle">INQUIRIES</div>
-                        <div classname="contInqEmail">LNYoptometry@gmail.com</div>
-                        <img className="contInqImg" src={process.env.PUBLIC_URL + "/images/desk-view.jpg"} alt="desk-view" />
+                        <div className="contInqEmail">
+                            <a className="locLink" href="mailto:LNYoptometry@gmail.com">LNYoptometry@gmail.com</a></div>
+                        <div className="col-12">
+                            <img className="contInqImg img-fluid" src={process.env.PUBLIC_URL + "/images/desk-view.jpg"} alt="desk-view" />
+                        </div>
                     </MDBCol>
-                    <MDBCol className="emailForm col-12 col-lg-8 col-md-8 col-sm-8">
-                        <div class="container form-top">
-                            <div class="row">
-                                <div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
-                                    <div class="panel panel-danger">
-                                        <div class="panel-body">
-                                            <form id="reused_form">
-                                                <div class="form-group">
-                                                    <label><i class="fa fa-user" aria-hidden="true"></i> Name</label>
-                                                    <input type="text" name="name" class="form-control" placeholder="Enter Name" />
-                                                </div>
-                                                <div class="form-group">
-                                                    <label><i class="fa fa-envelope" aria-hidden="true"></i> Email</label>
-                                                    <input type="email" name="email" class="form-control" placeholder="Enter Email" />
-                                                </div>
-                                                <div class="form-group">
-                                                    <label><i class="fa fa-comment" aria-hidden="true"></i> Message</label>
-                                                    <textarea rows="3" name="message" class="form-control" placeholder="Type Your Message"></textarea>
-                                                </div>
-                                                <div class="form-group">
-                                                    <button class="btn btn-raised btn-block btn-danger">Post &rarr;</button>
-                                                </div>
-                                            </form>
-                                            <div id="error_message">
-                                                <h4>
-                                                    Error
-                                                </h4>
-                                                Sorry there was an error sending your form.
-                                            </div>
-                                            <div id="success_message">
-                                                <h2>Success! Your Message was Sent Successfully.</h2>
-                                            </div>
+
+                    <MDBCol className="emailForm col-12 col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                        <div className="container">
+                            <div>
+                                <form ref={form} onSubmit={sendEmail}>
+
+                                    Name *
+                                    <div className="formRow">
+
+                                        <div className="form-group formBoxLeft col-6">
+                                            <input
+                                                type="text"
+                                                name="first_name"
+                                                className="form-control"
+                                                id="first_name"
+                                                required
+                                            />
+                                            <label className="nameLabel" for="first_name">First Name</label>
                                         </div>
+
+                                        <div className="form-group formBoxRight col-6">
+                                            <input
+                                                type="text"
+                                                name="last_name"
+                                                className="form-control"
+                                                id="last_name"
+                                                required
+                                            />
+                                            <label className="nameLabel" for="last_name">Last Name</label>
+                                        </div>
+
                                     </div>
-                                </div>
+
+                                    <div className="formRow">
+
+                                        <div className="form-group formBoxLeft col-6">
+                                            <label for="email">Email Address *</label>
+                                            <input
+                                                type="email"
+                                                name="email"
+                                                class="form-control"
+                                                id="email"
+                                                required
+                                            />
+                                        </div>
+
+                                        <div className="form-group formBoxRight col-6">
+                                            <label for="phone_number">Phone Number *</label>
+                                            <input
+                                                type="tel"
+                                                name="phone_number"
+                                                className="phone-format form-control"
+                                                id="phone_number"
+                                                required
+                                            />
+                                        </div>
+
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label for="subject">Subject *</label>
+                                        <input
+                                            type="tel"
+                                            name="subject"
+                                            className="form-control"
+                                            id="subject"
+                                            required
+                                        />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label for="message">Message *</label>
+                                        <textarea
+                                            type="text"
+                                            name="message"
+                                            className="form-control"
+                                            id="message"
+                                            rows="4"
+                                            required
+                                        ></textarea>
+                                    </div>
+
+                                    <button type="submit" className="btn btn-dark">
+                                        SUBMIT
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </MDBCol>
